d7c84381fed8808ec60c56abc2d65b374c0c8173
i-tsvetkov <i.tsvetkov.4@gmail.com>
2015-09-23 22:05:21 +0300

    added notification

--- check-net.sh
+++ check-net.sh
@@ -4,6 +4,7 @@ while :
 do
   ping -c1 8.8.8.8
   if [ $? -eq 0 ]; then
+    notify-send 'Internet is Up!' -i internet
     paplay /usr/share/sounds/freedesktop/stereo/alarm-clock-elapsed.oga
     break
   fi
