5a9a0cbf89adb61f549c0443be854b356f693101
i-tsvetkov <i.tsvetkov.4@gmail.com>
2016-04-17 18:22:49 +0300

    :beetle:: added empty line to separate body from headers

--- mail2sms.rb
+++ mail2sms.rb
@@ -4,7 +4,7 @@ def mail2sms(user, pass, phone, msg)
   smtp = Net::SMTP.new('smtp.gmail.com', 587)
   smtp.enable_starttls
   smtp.start('gmail.com', "#{user}@gmail.com", pass, :login)
-  smtp.send_message(msg, "#{user}@gmail.com", "#{phone}@sms.mtel.net")
+  smtp.send_message("\r\n#{msg}", "#{user}@gmail.com", "35988#{phone}@sms.mtel.net")
   smtp.finish
 end
 
